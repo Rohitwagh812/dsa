fca1451e1b0713699f407c3a6ac38e5274f97f3a
Rohitwagh812 <rohitwagh480@gmail.com>
2023-12-13 00:38:01 +0530

    some message

---
+++ sorting word.js
@@ -0,0 +1,9 @@
+// sorting word 
+
+const words = ['cat' , 'dog' , 'bird' , 'elephant' , 'tiger' , 'zebra' , 'monkey'];
+
+function sortWords(words) {
+    return words.sort();
+}
+
+console.log(sortWords(words));
