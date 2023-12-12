ce89375489fa3006e547a7fe5948ea76b0dc813e
Rohitwagh812 <rohitwagh480@gmail.com>
2023-12-13 00:30:00 +0530

    some message

---
+++ socket.js
@@ -0,0 +1,17 @@
+// target numbar 9
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const target = 10;
+
+function findPair(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        console.log(arr[i], arr[j]);
+      }
+    }
+  }
+}
+
+findPair(arr, target);
