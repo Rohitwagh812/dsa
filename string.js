fdc19c466cfe3aa458905f9b311322946356b8e8
Rohitwagh812 <rohitwagh480@gmail.com>
2023-12-13 00:24:33 +0530

    some message

--- string.js
+++ string.js
@@ -7,9 +7,10 @@ const reverseWord = (str) => {
     return str.split(' ').map(word => word.split('').reverse().join('')).join(' ')
 }
 
+console.log( '1' , reverseWord(string))
 
 // 2
 const reverse = string.split('').reverse().join('')
 const reverseWords = reverse.split(' ').reverse().join(' ')
 
-console.log(reverseWords)
+console.log( '2' , reverseWords)
