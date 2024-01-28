dfd1b08f4c6b0aa0fdc3ac4392bc14517d00fc98
Rohitwagh812 <rohitwagh480@gmail.com>
2024-01-29 00:00:54 +0530

    first commit

---
+++ palindrome.js
@@ -0,0 +1,22 @@
+  
+  
+
+// Palindrome in js 
+// Example of a palindrome
+// Redar === reverse Of REDAR
+
+const  isPolondrome = (str) => {
+
+    // convert string into array
+    // reverse the array
+    // convert it back to string 
+    // join the array into string
+    // compare the string with the original string
+    // if they are equal return true else false
+
+    return str.split('').reverse().join('') === str;
+}
+
+console.log('level' , isPolondrome('level'))
+console.log('salman' , isPolondrome('salman'))
+
--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 // var test = function () {
 //     console.log(x)
 
-//     var x =20
+//     // var x =20
 // }
 
 // test()
@@ -23,24 +23,67 @@
 // makeTeaAsync()
 
 
-/////
+// /////
 
 
-let cab = true
+// let cab = true
 
-if(!cab){
-    console.log("Cab is not available")
-}else{
-    console.log("Cab is available")
+// if(!cab){
+//     console.log("Cab is not available")
+// }else{
+//     console.log("Cab is available")
+// }
+
+// ///
+
+// let myArr1 = ['cat' , 'apple' , 'elephant' , 'ball' , 'dog']
+// //one 
+// console.log(myArr1.sort())
+
+// //  two
+// console.log(
+//     myArr1.sort((a, b)=>{
+//     // return a.localeCompare(b);
+//     // return b.localeCompare(a);
+
+//     return b - a
+// })
+// )
+
+
+// ///
+
+// let a = {} , 
+//     b= {key : 'b'} ,
+//     c = {key : 'c'};
+
+// a[b] = 123;
+// a[c] = 456;
+
+// console.log(a[b])
+
+//
+
+
+let person = {
+    name: "rohit wagh",
+    occupation: null , 
+    isFemale: false,
+    isMale: true,
+    lastName: ''
 }
-
-let myArr1 = ['cat' , 'apple' , 'elephant' , 'ball' , 'dog']
-
-console.log(myArr1.sort())
-
-
-console.log(
-    myArr1.sort((a, b)=>{
-    return a.localeCompare(b)
-})
-)
+ if(!person.occupation){
+    console.log('java' , person.occupation)
+ }
+ if(!person.isFemale){
+    console.log('python' , person.isFemale)
+ }
+ if(!person.isMale){
+    console.log('javascript' , person.isMale)
+ }
+ if(!person.lastName){
+    console.log('react' , person.lastName)
+ }
+ if(!person.abc){
+    console.log('angular' , person.abc)
+ }
